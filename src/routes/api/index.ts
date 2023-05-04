431f2961c51280df2514e754cfdec67be058f547
ClarenceDan <tangdan027@gmail.com>
2023-05-04 17:15:08 +0800

    Update index.ts

--- src/routes/api/index.ts
+++ src/routes/api/index.ts
@@ -62,11 +62,13 @@ export async function POST({ request }: APIEvent) {
     const { messages, key = localKey, temperature, password, model } = body
 
     if (passwordSet && password !== passwordSet) {
-      throw new Error("密码错误，请联系网站管理员。")
+      throw new Error(
+        "密码错误，请前往公众号：**[Aivesa](https://sourl.cn/9mwPzL)** 回复 **密码** 免费获取密码"
+      )
     }
 
     if (!messages?.length) {
-      throw new Error("没有输入任何文字。")
+      throw new Error("好像没有输入任何文字呢？")
     } else {
       const content = messages.at(-1)!.content.trim()
       if (content.startsWith("查询填写的 Key 的余额")) {
