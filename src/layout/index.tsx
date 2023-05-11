de41aa56ccf18f200432dd6718247e09a471d8fb
Clarence Dan <48417261+ClarenceDan@users.noreply.github.com>
2023-05-11 20:20:41 +0800

    Update index.tsx

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -5,6 +5,7 @@ export default function ({ children }: { children: JSXElement }) {
   return (
     <div id="root" class="sm:pt-8em py-2em before">
       <Header />
+      <script async src="https://umami.appbox.fun/script.js" data-website-id="4d2716ae-e1cc-405e-b894-85fd18da66fe"></script>
       {children}
     </div>
   )
